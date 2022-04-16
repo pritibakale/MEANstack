5bde59b6f5677d6cd7a7215309c83c39019002e5
Priti Bakale <bakalepriti1@gmail.com>
2022-04-16 23:30:32 +0530

    connected service file to post-list component

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
 import {Post} from './app.post.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
   private posts: Post[] =[];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts(){
-
+    return [...this.posts];
   }
 
   addPostService(title: string, content: string){
   const post: Post ={title: title, content: content};
   this.posts.push(post);
   console.log("*****************************",post);
+  this.postsUpdated.next([...this.posts]);
   }
-
+ getPostUpdateListner()
+ {
+   return this.postsUpdated.asObservable();
+ }
   constructor() { }
 }
